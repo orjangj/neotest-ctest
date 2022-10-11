7077d4b4799d83713541d57a50a2a7a8186906fc
Ørjan Gjengedal <og@datarespons.no>
2022-10-12 00:13:31 +0200

    Fix test_filter tests

--- lua/neotest-ctest/utils.lua
+++ lua/neotest-ctest/utils.lua
@@ -10,10 +10,12 @@ M = {}
 M.filter_tests = function(root, position)
   local test_filter = {}
   local result = 0
+  local data = position:data()
+  local type = data.type
 
-  if position.type == "test" or position.type == "namespace" then
-    test_filter[#test_filter + 1] = "-R " .. position.name
-  elseif position.type == "file" then
+  if type == "test" or type == "namespace" then
+    test_filter[#test_filter + 1] = "-R " .. data.name
+  elseif type == "file" then
     -- In contrast to ctest's -R option (which is used for selecting tests by regex pattern),
     -- the -I option gives more fine-grained control as to which test to execute based on
     -- unique test indexes. However, we do not know the test indexes contained in a file
@@ -21,18 +23,23 @@ M.filter_tests = function(root, position)
     -- about all available tests, and then infer the test index by comparing the test
     -- name in the output with the discovered positions in the file. Note that -I option
     -- can be specified multiple times, which makes this suitible for filtering tests.
-    -- TODO: Might want to consider vim.jobstart instead. The ctest output can be quite large.
     local output
-    result, output = lib.process.run("ctest", {
-      "--test-dir " .. root .. "/build",
-      "--show-only=json-v1",
-    })
+    local command = "ctest --test-dir " .. root .. "/build --show-only=json-v1"
+
+    -- TODO: Might want to consider vim.jobstart instead. The ctest output can be quite large.
+    result, output = lib.process.run({ "sh", "-c", command }, { stdout = true })
 
     if result == 0 then
+      assert(output.stdout ~= nil, ("Got empty json response from command `%s`"):format(command))
       local json_info = vim.json.decode(output.stdout)
-      for index, test in ipairs(json_info.tests) do
-        if test.name == position.id then
-          test_filter[#test_filter + 1] = "-I " .. index
+      for index, test_case in ipairs(json_info.tests) do
+        -- Compare test_case.name with any of the file.namespace.test id's
+        for _, namespace in ipairs(position:children()) do
+          for _, test in ipairs(namespace:children()) do
+            if test_case.name == test:data().id then
+              test_filter[#test_filter + 1] = "-I " .. index
+            end
+          end
         end
       end
     else
@@ -44,10 +51,10 @@ M.filter_tests = function(root, position)
         )
       )
     end
-  elseif position.type == "suite" then
+  elseif type == "suite" then
     -- NOTE: No need to specify filters since we're running all tests
   else
-    logger.warn(("%s: running %ss isn't supported"):format(require("neotest-ctest").name, position.type))
+    logger.warn(("%s: running %ss isn't supported"):format(require("neotest-ctest").name, type))
   end
 
   return result, test_filter
--- tests/unit/utils_spec.lua
+++ tests/unit/utils_spec.lua
@@ -10,8 +10,8 @@ describe("neotest-ctest", function()
   async.it("plugin.filter_tests (type=test)", function()
     local testfile = vim.loop.cwd() .. "/tests/unit/data/src/test.cpp"
     local root = vim.loop.cwd() .. "/tests/unit/data/"
-    local positions = plugin.discover_positions(testfile):to_list()
-    local result, test_filter = utils.filter_tests(root, positions[2][2][1])
+    local tree = plugin.discover_positions(testfile)
+    local result, test_filter = utils.filter_tests(root, tree:children()[1]:children()[1])
 
     assert.equals(result, 0)
 
@@ -22,8 +22,8 @@ describe("neotest-ctest", function()
   async.it("plugin.filter_tests (type=namespace)", function()
     local testfile = vim.loop.cwd() .. "/tests/unit/data/src/test.cpp"
     local root = vim.loop.cwd() .. "/tests/unit/data/"
-    local positions = plugin.discover_positions(testfile):to_list()
-    local result, test_filter = utils.filter_tests(root, positions[2][1])
+    local tree = plugin.discover_positions(testfile)
+    local result, test_filter = utils.filter_tests(root, tree:children()[1])
 
     assert.equals(result, 0)
 
@@ -35,10 +35,13 @@ describe("neotest-ctest", function()
   -- This function call should instead return the preformatted ctestinfo.json
   async.it("plugin.filter_tests (type=file)", function()
     local testfile = vim.loop.cwd() .. "/tests/unit/data/src/test.cpp"
-    local root = vim.loop.cwd() .. "/tests/unit/data/"
-    local positions = plugin.discover_positions(testfile):to_list()
-    local test_filter = utils.filter_tests(root, positions[1])
+    local root = vim.loop.cwd() .. "/tests/integration"
+    local tree = plugin.discover_positions(testfile)
+    local result, test_filter = utils.filter_tests(root, tree)
+
+    assert.equals(result, 0)
+
     local expected_filter = { "-I 1", "-I 2", "-I 3" }
-    assert.are.same(test_filter, expected_filter)
+    assert.are.same(expected_filter, test_filter)
   end)
 end)
