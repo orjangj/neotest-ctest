87a26f50cc540e9b492aa463d5f1944334fcee4a
orjangj <orjan.gjengedal@gmail.com>
2024-06-13 22:31:12 +0200

    Fix catch2 tests not being discovered inside namespaces (#10)

--- tests/unit/data/catch2/TEST_CASE_test.cpp
+++ tests/unit/data/catch2/TEST_CASE_test.cpp
@@ -1,6 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
 
+namespace TEST_CASE {
+
 TEST_CASE("First", "[first]") { REQUIRE(true); }
+
+} // namespace TEST_CASE
 
 TEST_CASE("Second", "[second]") {
   CHECK(false);
