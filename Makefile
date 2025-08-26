1112d02412ad463a6fb711947259d7fedafafbf2
orjangj <orjan.gjengedal@gmail.com>
2025-08-26 22:26:29 +0200

    chore: add GitHub issue templates
    Additionally updated test workflow to use sandboxed test environment for
    both local and CI execution.
    
    Resolves: #31

--- Makefile
+++ Makefile
@@ -1,15 +1,26 @@
-.PHONY: test unit integration
+.PHONY: setup test unit integration
+
+SANDBOX?=.sandbox
+
+export XDG_CACHE_HOME=${SANDBOX}/cache
+export XDG_CONFIG_HOME=${SANDBOX}/config
+export XDG_DATA_HOME=${SANDBOX}/data
+export XDG_STATE_HOME=${SANDBOX}/state
 
 MINIMAL_INIT = tests/unit/minimal_init.lua
 PLENARY_OPTS = {minimal_init='${MINIMAL_INIT}', sequential=true}
 
+setup:
+	mkdir -p ${XDG_CACHE_HOME} ${XDG_CONFIG_HOME} ${XDG_DATA_HOME} ${XDG_STATE_HOME}
+	nvim --headless -u ${MINIMAL_INIT} -c "TSInstallSync lua cpp c" -c q
+
 test: unit integration ;
 
 unit:
-	nvim --headless -c "PlenaryBustedDirectory tests/unit ${PLENARY_OPTS}"
+	nvim --headless -u ${MINIMAL_INIT} -c "PlenaryBustedDirectory tests/unit ${PLENARY_OPTS}"
 
 integration: build
-	nvim --headless -c "PlenaryBustedDirectory tests/integration ${PLENARY_OPTS}"
+	nvim --headless -u ${MINIMAL_INIT} -c "PlenaryBustedDirectory tests/integration ${PLENARY_OPTS}"
 
 build: tests/integration
 	$(MAKE) -C tests/integration build
--- tests/unit/minimal_init.lua
+++ tests/unit/minimal_init.lua
@@ -1,10 +1,44 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-local plugins = { "plenary.nvim", "nvim-dap", "nvim-treesitter", "neotest", "nvim-nio" }
-
-vim.notify = print
-vim.opt.swapfile = false
-vim.opt.rtp:append(".")
-for _, plugin in ipairs(plugins) do
-  vim.opt.rtp:append(lazypath .. "/" .. plugin)
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+
+vim.opt.rtp:prepend(lazypath)
+vim.opt.loadplugins = true
+
+local workspace = os.getenv("GITHUB_WORKSPACE")
+if not workspace then
+  -- NOTE: Fallback is intended for local development
+  local this_file = debug.getinfo(1, "S").source:sub(2) -- remove leading @
+  workspace = vim.fn.fnamemodify(this_file, ":h")
+end
+
+require("lazy").setup({
+  "nvim-neotest/neotest",
+  commit = "52fca6717ef972113ddd6ca223e30ad0abb2800c", -- see: https://github.com/nvim-neotest/neotest/issues/531
+  dependencies = {
+    { "nvim-neotest/nvim-nio" },
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-treesitter/nvim-treesitter" },
+    { "orjangj/neotest-ctest" },
+  },
+  opts = function()
+    local ts_config = require("nvim-treesitter.configs")
+    ts_config.setup({ ensure_installed = { "c", "cpp", "lua" } })
+    return { adapters = { require("neotest-ctest") } }
+  end,
+}, {
+  dev = {
+    path = vim.fn.fnamemodify(workspace, ":h"),
+    patterns = { "neotest-ctest" },
+    fallback = true,
+  },
+})
